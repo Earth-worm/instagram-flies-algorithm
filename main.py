5037e5e08e237175771fb185c991be84e4e034ff
Earth-worm <mycatiscute01082@gmail.com>
2023-06-29 00:02:06 +0900

    add(clustering)

--- main.py
+++ main.py
@@ -2,103 +2,118 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
-np.random.seed(874)
-
-x = np.r_[np.random.normal(size=1000,loc=0,scale=1),
-          np.random.normal(size=1000,loc=4,scale=1)]
-
-
-y = np.r_[np.random.normal(size=1000,loc=10,scale=1),
-          np.random.normal(size=1000,loc=10,scale=1)]
-
-data = np.array([x,y])
-
-
-"""
-p = plt.subplot()
-p.scatter(data[:,0],data[:,1],c="black",alpha=0.5)
-p.set_aspect("equal")
-plt.show()
-"""
-model= KMeans(2,init=centers)
-fit2 = model.fit(data.T)
-print(fit2.cluster_centers_)
-fig, ax = plt.subplots(1,2,dpi=140,figsize=(8,4),sharex=True,sharey=True)
-ax[0].set(xlabel="x",ylabel="y")
-ax[0].scatter(x,y,c=fit2.labels_,marker='o',alpha=0.5,s=55,linewidths=.1,edgecolor="k",cmap="turbo")
-ax[0].scatter(fit2.cluster_centers_[:,0],fit2.cluster_centers_[:,1],s=60, marker='D',c='pink')
-ax[0].set_title("n_clusters=5")
-#plt.savefig("kmeans_1_2.png",dpi=100)
-plt.show()
-
-"""
-max_iter = 200
-n_clusters = 2
-clusters = np.random.randint(0,n_clusters,data.shape[0])
-
-for _ in range(max_iter):
-    centroids = np.array([data[clusters == n,:].mean(axis=0) for n in range(n_clusters)])
-    new_clusters = np.array([np.linalg.norm(data-c,axis=1) for c in centroids]).argmin(axis = 0)
-
-    for n in range(n_clusters):
-        if not np.any(new_clusters == n):
-            centroids[n] = data[np.random.choice(data.shape[0],1),:]
-
-    if np.allclose(clusters,new_clusters):
-        break
-    clusters = new_clusters
-
-p = plt.subplot()
-p.scatter(data[clusters==0,0],data[clusters==0,1],c="red")
-p.scatter(data[clusters==1,0],data[clusters==1,1],c="white",edgecolors="black")
-p.scatter(centroids[:,0],centroids[:,1],c="orange",marker="s")
-
-p.set_aspect("equal")
-plt.show()
-
-"""
-n_clusters = 2
-
-
-
-class Files:
-    likes = None
-    labels = None
-    vectors = None # np.array([x1,x2,x3,...]) x1: np.array
-
 class InstaGramFlies:
     n_iters = None
     n_clusters = None
     n_flies = None
+    n_features = None
+
     centers = None
-    files = None #np.array(Files)
+    # best fly in in each cluster
+    best_fly_indices = None
+    cluster_like_average = None
+    center_dist_average = None
 
+    likes = None
+    labels = None
+    # np.array([[pioneer rate, faddist rate, master rate],[...],...])
+    strategies = None
+    vectors = None  # np.array([x1,x2,x3,...]) x1: np.array
 
-    def InstaGramFlies(self,n_iters,n_clusters,n_flies):
+    def __init__(self, n_iters, n_clusters, n_flies):
         self.n_iters = n_iters
         self.n_clusters = n_clusters
-        self.n_files = n_files
-        self.x = x
-        self.y = y
-        self.initFlies()
-        self.CalcLikes()
-    
-    # 対象問題ごとに初期化
-    def InitFiles(self):
-        self.files = Flies()
-        self.filies.vectors = np.zeros([n_flies,2])
+        self.n_flies = n_flies
+        self.InitFlies()
+        self.EvaluateLikes()
 
-    def CalcLikes(self):
-        for i in range(self.n_clusters):
-            self.flies.likes[i] = self.files.vectors[0]*self.files.vectors[1]
+    def InitFlies(self):
+        self.vectors = np.random.uniform(0, 100, [self.n_flies, 2])
+        self.strategies = np.zeros([self.n_flies, 3])
+        for i in range(self.n_flies):
+            randoms = np.random.uniform(1, 100, 3)
+            for j in range(3):
+                self.strategies[i][j] = randoms[j]/sum(randoms)
+        self.likes = np.zeros(self.n_flies)
+        self.n_features = len(self.vectors[0])
+        self.best_fly_indices = np.zeros(self.n_clusters)
 
-    def Clustering():
-        if centers is None:
-            model = KMeans(n_clusters,'k-means++')
+    def EvaluateLikes(self):
+        for i in range(self.n_flies):
+            self.likes[i] = self.vectors[i][0]*self.vectors[i][1]
+
+    def Clustering(self):
+        if self.centers is None:
+            model = KMeans(n_clusters=self.n_clusters)
         else:
-            model = KMeans(n_clusters,'k-means++',init = self.centers)
-        result = model.fit(vec_flies,init)
-        self.flies.labels = result.labels_
+            model = KMeans(n_clusters=self.n_clusters, init=self.centers)
+        result = model.fit(self.vectors)
+        self.labels = result.labels_
         self.centers = result.cluster_centers_
 
-    #def UpdateVectors
+        # best flies in each cluster
+        best = np.zeros(self.n_clusters)
+        self.cluster_like_average = np.zeros(self.n_clusters)
+        for i in range(self.n_flies):
+            label = self.labels[i]
+            if (self.likes[i] > best[label]):
+                best[label] = self.likes[i]
+                self.best_fly_indices[label] = i
+
+        # like average in each cluster
+        for i in range(self.n_clusters):
+            self.cluster_like_average[i] = np.mean(
+                [self.likes[j] for j in range(self.n_flies) if self.labels[j] == i])
+
+        # average dist between each cluster
+        self.center_dist_average = 0
+        for i in range(self.n_clusters):
+            for j in range(i+1,self.n_clusters):
+                self.center_dist_average += np.linalg.norm(self.centers[i]-self.centers[j])
+        self.center_dist_average /= sum(range(1,self.n_clusters))
+
+                
+
+    def UpdateFlieVector(self):
+        randoms = np.random.rand(self.n_flies)
+        sum = 0
+        for i in range(self.n_flies):
+            action = 0
+            for a in range(3):
+                action = a
+                sum = sum + self.strategies[i]
+                if sum > randoms[i]:
+                    break
+            # pioneer
+            if action == 0:
+                self.vectors[i] = self.UpdatePioneer(self.vectors[i])
+            # faddist
+            if action == 1:
+                self.vectors[i] = self.UpdateFaddist(self.vectors[i])
+            # master
+            if action == 2:
+                self.vectors[i] = self.UpdateMaster(
+                    self.vectors[i], self.labels[i])
+
+    def UpdatePioneer(self, vector):
+        dist = 0
+        #TODO kokokara!!! averageを使ってUpdatePioneerを更新
+        return 100
+
+    def UpdateFaddist(self, vector):
+        return 100
+
+    def UpdateMaster(self, vector, label):
+        center = self.centers[label]
+        return 100
+
+
+if __name__ == "__main__":
+    n_clusters = 3
+    n_flies = 10
+    instaFlies = InstaGramFlies(100, n_clusters, n_flies)
+    instaFlies.Clustering()
+    print(instaFlies.centers,instaFlies.center_dist_average)
+    plt.scatter(instaFlies.centers.T[0], instaFlies.centers.T[1], marker="*")
+    plt.scatter(instaFlies.vectors.T[0], instaFlies.vectors.T[1], marker=".")
+    plt.show()
